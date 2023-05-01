99fd3569c1610302fe74137f6b320c0a9407193e
mayiPalacios <00018619@uca.edu.sv>
2023-05-01 12:33:15 -0600

    fix vercel problem

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,4 @@
-import { FC } from "react";
-import { PageProps } from "../../../.next/types/app/layout";
 import Footer from "lastHomework/components/footer";
-import { useRouter } from "next/router";
 import LoginPage from "lastHomework/components/login";
 
 const page = ({}) => {
--- src/app/meDetails/page.tsx
+++ src/app/meDetails/page.tsx
@@ -1,10 +1,8 @@
 "use client";
-import { FC } from "react";
-import { PageProps } from "../../../.next/types/app/layout";
 import Footer from "lastHomework/components/footer";
 import Header from "lastHomework/components/header";
 
-const page: FC<PageProps> = ({}) => {
+const page = ({}) => {
   return (
     <>
       <Header />
